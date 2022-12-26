2460258ec0c95e8d840381d6c62ea063e04728f1
andre-min <andre.minato610@gmail.com>
2022-12-26 15:21:59 -0300

    feat: First interaction

---
+++ src/infra/swapi_api_consumer.py
@@ -0,0 +1,9 @@
+import requests
+
+class SwapiApiConsumer():
+    @classmethod
+    def get_starships(self, page: int) -> any:
+        params = {'page': page}
+        response = requests.get('http://swapi.dev/api/starships/', params=params)
+
+        return response.json()
